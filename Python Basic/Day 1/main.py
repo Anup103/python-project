8da1ac8b526f70f92460bc8632d8a5bd87d97015
Anup Giri <anupgiri007@gmail.com>
2021-06-01 13:08:02 -0500

    day 1

---
+++ Python Basic/Day 1/main.py
@@ -0,0 +1,8 @@
+print('name = Anup Giri')
+print('age = 26')
+print("height = 6'2")
+print('weight = 64 pound')
+
+print('*/20')
+print('"hey,How are you?"')
+
---
+++ Python Basic/Day 1/variable.py
@@ -0,0 +1,25 @@
+name = "Sagar"
+print(name)
+
+name = "Anup Giri"
+
+age = "23"
+print(age)
+
+print('my name is ' + name + '.' + age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
