3c79770cf780356a4ec065260ffb82cfe6263d3b
Anup Giri <anupgiri007@gmail.com>
2021-06-02 00:23:57 -0500

    message assignment

--- Python Basic/Day 1/variable.py
+++ Python Basic/Day 1/variable.py
@@ -8,6 +8,34 @@ print(age)
 
 print('my name is ' + name + '.' + age)
 
+# Assignment day 1
+name = 'His name is Sagar Budathoki'
+print(name)
+
+age = 'His age is 22'
+print(age)
+
+Hobby = 'His hobby is to listen to underground rap music'
+print(Hobby)
+
+Country = 'He lives in Nepal'
+print('Country')
+
+print(name + '.' + age + '.' + Hobby + '.' + Country + '.')
+
+# Another model
+relation = 'Son pocket money'
+Sunday = int("3")
+Monday = int("4")
+Tuesday = int("20")
+print(relation + ';''Total=', '$', Sunday + Monday + Tuesday)
+
+
+
+
+
+
+
 
 
 
