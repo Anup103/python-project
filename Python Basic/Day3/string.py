269bb0e07b2c2979b85f9e3162376c15e083c92b
Anup Giri <anupgiri007@gmail.com>
2021-06-06 00:06:32 -0500

    little dif.

---
+++ Python Basic/Day3/string.py
@@ -0,0 +1,18 @@
+# Boolean.py
+a = True
+b = False
+print(type(a))
+print(type(b))
+
+c = not a
+print(c)
+
+t = True
+f = False
+
+print(t and f)
+print(t or f)
+# false and false = false (point noted below)
+print(t or f and f or t)
+print(not t)
+
