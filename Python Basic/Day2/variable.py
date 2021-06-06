fdbe78b8e9eeef7ddaca51476c847bd18b772fd4
Anup Giri <anupgiri007@gmail.com>
2021-06-05 23:48:29 -0500

    space

--- Python Basic/Day2/variable.py
+++ Python Basic/Day2/variable.py
@@ -1,10 +1,9 @@
-first_name = input('What is your first name? ')
-last_name = input('What is your last name?')
-full_name = first_name + last_name
-
-print('full_name')
-age = str(22)
-print(full_name + age)
+first_name = input('What is your first name?' ' ')
+# positioning space after ?
+last_name = input('What is you Last name?' ' ')
+# ""
+full_name = (first_name + ' ' + last_name)
+print(full_name)
 
 
 
