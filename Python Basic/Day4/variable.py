6d9c1501acb7f1c195fc7175713d984821239ded
Anup Giri <anupgiri007@gmail.com>
2021-06-08 23:43:50 -0500

    Assignment 1

---
+++ Python Basic/Day4/variable.py
@@ -0,0 +1,18 @@
+# assingment 1
+Father_name = 'Rajesh Pudasaini'
+Mother_name = 'Sima Khatri'
+Father_name.lower()
+
+a = (Father_name[0:3].lower())
+b = (Mother_name[0:3])
+print('Children_name ''=', b + a)
+
+
+
+
+
+
+
+
+
+
