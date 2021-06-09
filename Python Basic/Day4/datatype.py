f686565a8eb522625450b2563561fee5803a9c13
Anup Giri <anupgiri007@gmail.com>
2021-06-09 00:36:53 -0500

    Data tyoes Assigntment

---
+++ Python Basic/Day4/datatype.py
@@ -0,0 +1,82 @@
+# Datatype 'if'
+a = 19
+print(type(a))
+
+a = 19.
+print(type(a))
+
+b = (9)
+c = '(9)'
+d = '9'
+print(type(b))
+print(type(c))
+print(type(d))
+
+# cont... 'if' Boolean
+a = False
+print(type(a))
+
+# if.....tuple
+a = (1, 2, 3)
+print(type(a))
+
+# if-----(LIST)
+a = [1, 2, 3]
+print(type(a))
+
+
+# if-----DICTIONARY
+a = {'key': 19}
+print(type(a))
+
+# if----(Complex)
+a = 1 + 23j
+print(type(a))
+
+# if----(set)
+a = {1, 2, 3}
+print(type(a))
+
+# ___NUMERIC DATA TYPE___
+# if ---
+a = 1
+b = 2
+print(type(a/b))
+
+a = 1
+b = 2
+print(type(a*b))
+
+# if----- (integer)
+a = 5.1
+b = 0
+c = 3
+print(type(a + b and c))
+
+# if---(string)
+a = 5.1
+b = 0
+c = '3'
+print(type(a + b and c))
+
+
+# Floating number converted false boolean in code
+a = 3.6
+b = 2
+result = (a < b)
+print(type(result))
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
